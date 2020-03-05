29a2b69d686bd3abfe6baa01e5bf6755869c03ff
Jose E Reyes <jreyes8631@gmail.com>
2020-03-05 01:52:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,11 @@ def introduction(name)
  "Hi, my name is #{name} and I am learning to program in #{lenguage}."
   
 end
+
+
+
+def greeting(name)
+  "Hello, " + name + ". How are you doing?"
+end
+
+puts greeting("Bob")
